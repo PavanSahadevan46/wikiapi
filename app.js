dce8568bd230d8828066c6e46feca9824efa1dd6
pavan <511866@chrome.escg.ac.uk>
2021-12-01 11:37:51 +0000

    INital commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+mongoose.connect("mongodb:localhost:27017/wikiDB",{useNewUrlParser: true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, function() {
+console.log("Server started on port 3000");
+});
